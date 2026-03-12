604fffb2e8ae7cc34cc53ad0cef2e87b9bb0aae1
amrxtgh <ghimireamrit619@gmail.com>
2026-03-12 14:28:55 +0545

    add: lsp for c

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "rust_analyzer" }
+local servers = { "html", "cssls", "rust_analyzer", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+
